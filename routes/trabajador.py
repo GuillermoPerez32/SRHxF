ac77a8e990b3f76c2cf795a7867deef96e0e4e6f
GuillermoPerez32 <luisguillermo.rodriguez32@gmail.com>
2022-07-20 23:09:42 -0400

    added models

--- routes/trabajador.py
+++ routes/trabajador.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from app import app
+from config.db import conn
+from schemas.trabajador import TrabajadorIn, TrabajadorUpdate
+from models.trabajador import trabajadores
 
-router = APIRouter()
+@app.get('/trabajadores')
+async def get_trabajadores():
+  return conn.execute(trabajadores.select()).fetchall()
+
+@app.post('/trabajadores')
+async def create_trabajador(trabajador: TrabajadorIn):
+  return conn.execute(trabajadores.insert(trabajador.dict()))
+
+@app.put('/trabajadores/{id}')
+async def update_trabajador(id: int, trabajador: TrabajadorUpdate):
+  return conn.execute(trabajadores.insert(trabajador.dict()))
